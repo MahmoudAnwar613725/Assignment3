3987cf37dbed40e8212abe76716c252521cb1f15
MahmoudAnwar613725 <mahmohamed@miu.edu>
2022-10-29 14:45:51 -0500

    assignment3-lesson4 problem 1

--- app/src/main/java/com/course/assignment3/TableLayoutActivity.kt
+++ app/src/main/java/com/course/assignment3/TableLayoutActivity.kt
@@ -2,19 +2,28 @@ package com.course.assignment3
 
 import android.app.ActionBar
 import android.os.Bundle
+import android.view.View.inflate
 import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.setMargins
-import kotlinx.android.synthetic.main.activity_main.*
+import com.course.assignment3.databinding.ActivityMainBinding
+import com.course.assignment3.databinding.ActivityMainBinding.inflate
 
 
 class TableLayoutActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        btnAddRow.setOnClickListener {
+        binding = inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        binding.btnAddRow.setOnClickListener {
             if (validateFields()) {
-                addNewRow(edAndroidVersion.text.toString(), edAndroidCodeName.text.toString());
+                addNewRow(
+                    binding.edAndroidVersion.text.toString(),
+                    binding.edAndroidCodeName.text.toString()
+                )
             }
         }
 
@@ -22,17 +31,17 @@ class TableLayoutActivity : AppCompatActivity() {
 
     private fun validateFields(): Boolean {
 
-        if (edAndroidVersion.text.isEmpty()) {
+        if (binding.edAndroidVersion.text.isEmpty()) {
             Toast.makeText(this, "Enter android code version please", Toast.LENGTH_SHORT).show()
-            return false;
+            return false
         }
 
-        if (edAndroidCodeName.text.isEmpty()) {
+        if (binding.edAndroidCodeName.text.isEmpty()) {
             Toast.makeText(this, "Enter android code name please", Toast.LENGTH_SHORT).show()
-            return false;
+            return false
         }
 
-        return true;
+        return true
     }
 
     private fun addNewRow(version: String, codeName: String) {
@@ -45,26 +54,28 @@ class TableLayoutActivity : AppCompatActivity() {
         tableRow.layoutParams = layoutParams
 
         // add values into row by calling addView()
-        val tvVersion = TextView(this,null,0,R.style.tableCell)
-        val tvCode = TextView(this,null,0,R.style.tableCell)
+        val tvVersion = TextView(this, null, 0, R.style.tableCell)
+        val tvCode = TextView(this, null, 0, R.style.tableCell)
 
-        tableRow.addView(tvVersion, 0);
-        tableRow.addView(tvCode, 1);
+        tableRow.addView(tvVersion, 0)
+        tableRow.addView(tvCode, 1)
 
-       //set margin
-        val lp = TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT,TableRow.LayoutParams.WRAP_CONTENT)
-        lp.setMargins(5,5,5,5)
+        //set margin
+        val lp = TableRow.LayoutParams(
+            TableRow.LayoutParams.WRAP_CONTENT,
+            TableRow.LayoutParams.WRAP_CONTENT
+        )
+        lp.setMargins(5, 5, 5, 5)
         tvVersion.layoutParams = lp
         tvCode.layoutParams = lp
 
-      tvVersion.text = version
+       //set values
+        tvVersion.text = version
         tvCode.text = codeName
 
 
-
-
         // Finally add the created row row into layout
-        tblAndroidData.addView(tableRow) // id from Layout_file
+        binding.tblAndroidData.addView(tableRow)
 
     }
 }
